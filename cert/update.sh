e4ea20f0d9e53e605e3a9ab5ba4f20e5b142f2a4
Sven Fischer <sven@leiderfischer.de>
2024-12-23 01:45:30 +0100

    Update blocky release

--- cert/update.sh
+++ cert/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-kubectl get secret -n cert-manager leiderfischer-de-tls --kubeconfig ~/.kube/bak/config --context struppi -o yaml \
+kubectl get secret -n clusterissuer leiderfischer-de-tls --context admin@external -o yaml \
     | sed "/resourceVersion:/d;/uid:/d;s/name: .*/name: certificate-issuer-domain-0-wildcard/;s/namespace:.*/namespace: traefik/" \
     | kubectl apply -f - --context admin@main
