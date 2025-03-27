59052ec1dcbe973b6c57b59bffcee0912a2f7b12
Sven Fischer <sven@leiderfischer.de>
2025-03-27 08:53:45 +0100

    Adapt the cert update script

--- cert/update.sh
+++ cert/update.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-kubectl get secret -n clusterissuer leiderfischer-de-tls --context admin@external -o yaml \
-    | sed "/resourceVersion:/d;/uid:/d;s/name: .*/name: certificate-issuer-domain-0-wildcard/;s/namespace:.*/namespace: traefik/" \
+# kubectl get secret -n clusterissuer leiderfischer-de-tls --context admin@external -o yaml \
+#     | sed "/resourceVersion:/d;/uid:/d;s/name: .*/name: certificate-issuer-domain-0-wildcard/;s/namespace:.*/namespace: traefik/" \
+#     | kubectl apply -f - --context admin@main
+
+kubectl get secret -n clusterissuer leiderfischer-de-tls --context admin@external -o json \
+    | jaq 'del(.metadata) | . += {metadata: {name:"certificate-issuer-domain-0-wildcard", namespace: "traefik"}}' \
     | kubectl apply -f - --context admin@main
